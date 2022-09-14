5be21d3f280ed2511ffe923827c4f38777e53f4a
mjy191 <mjy191@163.com>
2022-09-14 21:17:25 +0800

    增加手机号码解密错误码

--- src/Enum.php
+++ src/Enum.php
@@ -8,34 +8,37 @@ class Enum
      * 定义接口返回错误码
      */
     // 成功
-    const codeSuccess     = 1;
+    const codeSuccess      = 1;
     // 参数错误
-    const erCodeParam    = 2;
+    const erCodeParam      = 2;
     // 系统错误
-    const erCodeSystem   = 3;
+    const erCodeSystem     = 3;
     // 签名错误
-    const erCodeSign      = 4;
+    const erCodeSign       = 4;
+    // 小程序解密手机号码错误
+    const decryptPhoneError= 5;
     // 请求method方式错误
-    const erCodeMethod    = 100;
+    const erCodeMethod     = 100;
     // token错误
-    const erCodeToken     = 401;
+    const erCodeToken      = 401;
     // 接口404
-    const erCodeNotFound = 404;
+    const erCodeNotFound   = 404;
     // 服务器错误
-    const erCodeServer   = 500;
+    const erCodeServer     = 500;
 
     /**
      * 返回错误信息
      */
     const msg = [
-        self::codeSuccess      => 'success',
-        self::erCodeParam      => '参数错误',
-        self::erCodeSystem     => '系统错误',
-        self::erCodeSign       => 'sing error',
-        self::erCodeMethod     => '请求method方式错误',
-        self::erCodeToken      => 'token错误',
-        self::erCodeNotFound   => '404 not found',
-        self::erCodeServer     => '服务器错误',
+        self::codeSuccess       => 'success',
+        self::erCodeParam       => '参数错误',
+        self::erCodeSystem      => '系统错误',
+        self::erCodeSign        => 'sing error',
+        self::decryptPhoneError => '手机号码获取失败，请重新获取',
+        self::erCodeMethod      => '请求method方式错误',
+        self::erCodeToken       => 'token错误',
+        self::erCodeNotFound    => '404 not found',
+        self::erCodeServer      => '服务器错误',
     ];
 
     /**
